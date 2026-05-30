5f931ad9079ed7d4df4d92c2f97e1bb14213bb10
henristr <strickerhenri@gmail.com>
2026-05-30 13:40:59 +0200

    changed port

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ def socials():
     return render_template("socials.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8888)
+    app.run(debug=True, host="0.0.0.0", port=1313)
