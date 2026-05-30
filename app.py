75da2faa83c41a8cbddbb5893fb1c357689ac269
henristr <strickerhenri@gmail.com>
2026-05-30 13:28:41 +0200

    changed to flask and finished the side

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/socials")
+def socials():
+    return render_template("socials.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8888)
