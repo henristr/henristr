c302a391148b93c1528525a9e7db8e3149ba566a
henristr <strickerhenri@gmail.com>
2026-06-14 15:29:58 +0200

    added markdown support

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, abort
+from pathlib import Path
+from markdown import markdown
 
 app = Flask(__name__)
+
+PAGES_DIR = Path("pages")
+
+@app.route("/<page>")
+def wiki(page):
+    file = PAGES_DIR / f"{page}.md"
+
+    if not file.exists():
+        abort(404)
+
+    html = markdown(
+        file.read_text(encoding="utf-8")
+    )
+
+    html = f'<div class="markdown-content">{html}</div>'
+
+    return render_template("markdown_page.html", content=html)
 
 @app.route("/")
 def index():
