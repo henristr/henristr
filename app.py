fb2b677b7075f4508d6ef9b8b4e72934eb64111d
henristr <strickerhenri@gmail.com>
2026-06-24 19:31:16 +0200

    added url shortener

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, send_from_directory, abort
+from flask import Flask, render_template, send_from_directory, abort, redirect
 from markdown import markdown
 from pathlib import Path
+import json
 
 app = Flask(__name__)
 
@@ -8,13 +9,19 @@ PAGES_DIR = Path("pages")
 
 @app.route("/<page>")
 def md(page):
+    with open("links.json", "r", encoding="utf-8") as f:
+        items = json.load(f)
+    
+    item = next((i for i in items if i["path"] == page), None)
+    if item:
+        return redirect(item["url"])
+
     file = PAGES_DIR / f"{page}.md"
 
     if not file.exists():
         abort(404)
 
     content = file.read_text(encoding="utf-8")
-
     title = page
 
     for line in content.splitlines():
